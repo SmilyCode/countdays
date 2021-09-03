72f346284a119ae4077308708f2091bcd230e30c
smilykuy <smilykuy8@gmail.com>
2021-09-04 06:50:27 +0700

    added not null annotation and description to each method

--- src/main/java/smily/plugin/countdays/scoreboard/DayScoreboard.java
+++ src/main/java/smily/plugin/countdays/scoreboard/DayScoreboard.java
@@ -6,12 +6,22 @@ import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 
+import jakarta.validation.constraints.NotNull;
+
 public class DayScoreboard {
+    @NotNull
     protected Scoreboard dayScoreboard;
+
+    @NotNull
     protected Objective objective;
+
+    @NotNull
     protected Score dayScore;
+
+    @NotNull 
     protected int score;
 
+    //create scoreboard
     void createScoreboard(){
         dayScoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
 
@@ -20,6 +30,7 @@ public class DayScoreboard {
         dayScore.setScore(score);
     }
 
+    //set the scoreboard to player
     public void setScoreboardToPlayer(Player player){
         if (dayScoreboard != null){
             player.setScoreboard(dayScoreboard);
